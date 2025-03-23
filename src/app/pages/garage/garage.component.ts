938d031a225adc0dd72c9aacbc401e726a206505
nshan20 <nshan018@gmail.com>
2025-03-23 20:04:52 +0400

    Fix car name concatenation and adjust CSS styling
    - Remove the extra "+" in the random car name generation for cleaner output.
    - Update car-row CSS to remove background color for improved aesthetics.
    - Add top margin to material icons for better spacing.

--- src/app/pages/garage/garage.component.ts
+++ src/app/pages/garage/garage.component.ts
@@ -323,7 +323,7 @@ export class GarageComponent implements AfterViewInit, OnInit, OnDestroy {
     const requests = [];
 
     for (let i = 0; i < 100; i++) {
-      const randomName = carNames[Math.floor(Math.random() * carNames.length)] + " + "
+      const randomName = carNames[Math.floor(Math.random() * carNames.length)] + " "
         + carNames[Math.floor(Math.random() * carNames.length)];
       const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0").toUpperCase()}`;
 
